82ffabe62389fc6b1f761fbcd4d84ae1ce005a85
FALGUNI TIMANDE <150127240+Falguni1412@users.noreply.github.com>
2026-09-08 15:57:16 +0530

    Remove code block formatting from index.js
    Removed unnecessary code block formatting from index.js.

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,4 +1,4 @@
-```js
+
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
@@ -161,4 +161,3 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 module.exports = app;
-```
